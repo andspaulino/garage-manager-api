0c94f65223c3a672b3769d1be06c8754d72b7a29
Anderson Paulino <andspaulino@gmail.com>
2025-09-26 20:58:22 -0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "garage-manager-api"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/GreetingService.kt
@@ -0,0 +1,5 @@
+package io.github.andspaulino
+
+fun interface GreetingService {
+    fun sayHello(): String
+}
