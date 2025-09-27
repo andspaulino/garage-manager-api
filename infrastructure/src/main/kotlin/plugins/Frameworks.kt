726a634ceab2bd84392ccda22b8810bfc1b1dc40
Anderson Paulino <andspaulino@gmail.com>
2025-09-27 01:38:00 -0300

    feat: finalized multimodule

--- infrastructure/src/main/kotlin/plugins/Frameworks.kt
+++ infrastructure/src/main/kotlin/plugins/Frameworks.kt
@@ -4,16 +4,15 @@ import di.RepositoryModule
 import di.UseCasesModule
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
 fun Application.configureFrameworks() {
     install(Koin) {
         slf4jLogger()
-        modules(module {
-            RepositoryModule.module
+        modules(
+            RepositoryModule.module,
             UseCasesModule.module
-        })
+        )
     }
 }
--- infrastructure/src/main/kotlin/routes/UserRoutes.kt
+++ infrastructure/src/main/kotlin/routes/UserRoutes.kt
@@ -3,25 +3,28 @@ package routes
 import application.usecase.FindAllUsersUseCase
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.response.respond
-import io.ktor.server.routing.Routing
+import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 
-fun Routing.userRoutes(
-    findAllUsersUseCase: FindAllUsersUseCase,
+fun Route.userRoutes(
+    findAllUsersUseCase: FindAllUsersUseCase
 ) {
     route("/users") {
-        
-        get {
-            try {
-                val users = findAllUsersUseCase.execute()
-                call.respond(HttpStatusCode.OK, users)
-            } catch (e: Exception) {
-                call.respond(
-                    HttpStatusCode.InternalServerError, 
-                    mapOf("error" to "Failed to fetch users: ${e.message}")
-                )
-            }
+        findAll(findAllUsersUseCase)
+    }
+}
+
+private fun Route.findAll(findAllUsersUseCase: FindAllUsersUseCase) {
+    get {
+        try {
+            val users = findAllUsersUseCase.execute()
+            call.respond(HttpStatusCode.OK, users)
+        } catch (e: Exception) {
+            call.respond(
+                HttpStatusCode.InternalServerError,
+                mapOf("error" to "Failed to fetch users: ${e.message}")
+            )
         }
     }
 }
